7291d459bd22bd1ef920878584befc11d19ac96d
+stephen.nene <+>
2023-03-01 13:52:46 +0300

    in the todo contaroller....and wueeeh!!

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,4 +2,10 @@ class TodoController < Sinatra::Base
   get '/hello' do
     "Welcome to TodoController"
   end
+
+  post '/todos/create' do
+    data = request.body.read
+    data.to_s
+  end
+
 end
--- app/models/todos.rb
+++ app/models/todos.rb
@@ -1,3 +1,3 @@
 class Todo < ActiveRecord::Base
-  
+  enum :status, [ :CREATED, :ONGOING, :COMPLETED, :CANCELLED ]
 end
